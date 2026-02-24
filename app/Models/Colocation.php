b1d9b411465b302f1ff807ff10722f2a5fe5db08
kuklinski <mohammedmehdisaibat@gmail.com>
2026-02-24 21:08:45 +0000

    add Colocation Model properties and it's database table

---
+++ app/Models/Colocation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = ['name', 'description', 'owner_id'];
+
+    public function users() {
+        return $this->belongsToMany(User::class)->withPivot('role')->withTimestamps();
+    }
+
+    public function owner() {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
