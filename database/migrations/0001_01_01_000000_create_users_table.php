d68cab3d7b2dc51e9cfe53b001b55e6719fab6b9
kuklinski <mohammedmehdisaibat@gmail.com>
2026-02-25 20:29:53 +0000

    change role to global_role admin/user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('global_role', ['admin', 'user'])->default('user');
             $table->boolean('is_banned')->default(false);
             $table->integer('reputation')->default(0);
             $table->rememberToken();
