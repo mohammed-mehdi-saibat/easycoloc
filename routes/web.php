6ba824c7fd532d98ca8f6b8ecebde587116cfa5d
kuklinski <mohammedmehdisaibat@gmail.com>
2026-02-25 20:41:34 +0000

    routes with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,30 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\ColocationController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::middleware(['auth', 'verified', 'check.banned'])->group(function () {
     
-    
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Dashboard 
+    Route::get('/dashboard', [ColocationController::class, 'index'])->name('dashboard');
 
-    
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Colocations (Groups)
+    Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+    
+    // Invitations
+    Route::post('/colocations/{colocation}/invite', [InvitationController::class, 'send'])->name('invitations.send');
+    Route::get('/invitations/accept/{token}', [InvitationController::class, 'accept'])->name('invitations.accept');
 });
 
 require __DIR__.'/auth.php';
