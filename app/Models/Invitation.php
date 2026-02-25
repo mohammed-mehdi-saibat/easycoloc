6c412f6336d28d98ccc97c4fda13ded86acc596a
kuklinski <mohammedmehdisaibat@gmail.com>
2026-02-25 21:56:06 +0000

    refine invitation model and it's database table

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    protected $fillable = ['colocation_id', 'email', 'token', 'expires_at', 'accepted'];
+    protected $fillable = ['colocation_id', 'inviter_id', 'email', 'token', 'expires_at', 'accepted'];
+
+    protected $casts = ['expires_at' => 'datetime',];
 
     public function colocation() {
         return $this->belongsTo(Colocation::class);
+    }
+
+    public function inviter() {
+        return $this->belongsTo(User::class, 'inviter_id');
     }
 }
--- database/migrations/2026_02_24_205908_create_invitations_table.php
+++ database/migrations/2026_02_24_205908_create_invitations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('inviter_id')->constrained('users')->onDelete('cascade');
             $table->string('email');
             $table->string('token')->unique();
             $table->timestamp('expires_at');
